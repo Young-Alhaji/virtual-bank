0411bb47b6d964807e39e101cce6c6cdc163abc1
RIDWAN KOREDE <adesunkanmi123@gmail.com>
2023-07-27 00:36:29 +0100

    completed the footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -33,11 +33,33 @@ function Footer() {
            </ul>
         </div>
 
-        
+        <div className="col-sm-6 col-6 col-md-3">
+          <h6>Partner</h6>
+          <ul className="footer-links">
+            <li><a href="">Our Partner</a></li>
+            <li><a href="">Become a Partner</a></li>
+          </ul>
+        </div>
       </div>
       <hr className='brand-color'/>
     </div>
-    
+    <div className="container">
+      <div className="row">
+        <div className="col-md-8 col-sm-6">
+          <p className="copyright-text">Copyright &copy; 2022 All Rights Reserved by Oguntola Ridwan.
+          </p>
+        </div>
+
+        <div className="col-md-4 col-sm-6 col-12">
+          <ul className="social-icons">
+            <li><a className="facebook" href="#"><i className="fa fa-facebook"></i></a></li>
+            <li><a className="twitter" href="#"><i className="fa fa-twitter"></i></a></li>
+            <li><a className="dribbble" href="#"><i className="fa fa-dribbble"></i></a></li>
+            <li><a className="linkedin" href="#"><i className="fa fa-linkedin"></i></a></li>   
+          </ul>
+        </div>
+      </div>
+    </div>
 </footer>
 </div>
   )
